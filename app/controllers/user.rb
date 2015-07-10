f395dfe884f86754afcd1f0779b8730d9baaafbb
codeamac02 <codeamac02@gmail.com>
2015-07-10 13:38:04 -0500

    Validation for session and current_user

---
+++ app/controllers/user.rb
@@ -0,0 +1,102 @@
+before do 
+ session_validation
+ current_user_validation
+end
+
+# GETS ============================================================================
+
+# NEW
+get '/register' do
+
+	erb :new_user
+end
+
+# SHOW
+get '/users/:id' do
+	id = params[:id]
+	@user = User.find("#{id}")
+	erb :show_user
+end
+
+# EDIT
+get '/users/:id/edit' do
+	id = params[:id]
+	@user = User.find("#{id}")
+	erb :edit_user
+end
+
+get '/logout' do
+	session.clear
+	redirect to('/')
+end
+
+# POSTS ===========================================================================
+
+
+# LOGIN
+post '/login' do
+	email = params[:email]
+	password = params[:password]
+	user = User.authenticate(email, password)
+	if user
+		id = user.id
+		# session[:email] = params[:email]
+		session[:user_id] = id
+		redirect to("/users/#{id}")
+	else
+		session[:errors] = "Datos incorrectos"
+		erb :index
+	end
+end
+
+
+# CREATE
+post '/register' do
+	# first_name = params[:user][:first_name]
+	# last_name = params[:user][:last_name]
+	# username = params[:user][:username]
+	# email = params[:user][:email]
+	# password = params[:user][:password]
+
+
+	# @user = User.create(first_name: first_name, last_name: last_name, username: username, email: email, password: password)
+	@user = User.create(params[:user])
+	session[:user_id] = @user.id
+	redirect to("/users/#{@user.id}")
+end
+
+
+# UPDATE
+post '/users/:id' do
+	puts params
+	id = params[:id]
+	user = User.find(id)
+	
+
+	first_name = params[:user][:first_name]
+	last_name = params[:user][:last_name]
+	username = params[:user][:username]
+	email = params[:user][:email]
+	password = params[:user][:password]
+
+
+	if password == ""
+		user.update(first_name: first_name, last_name: last_name, username: username, email: email)	
+	else
+		# Meter un confirmación de el password anterior para poder cambiar el pasword
+		user.update(params[:user])	
+	end
+	redirect to("/users/#{id}")
+end
+
+# DELETE
+post '/users/:id/delete' do
+	id = params[:id]
+	user = User.destroy(id)
+	session.clear
+	redirect to ('/')
+end
+
+
+
+
--- app/helpers/user.rb
+++ app/helpers/user.rb
@@ -8,4 +8,25 @@ helpers do
   def logged_in?
     !current_user.nil?
   end
+
+  def current_user_validation
+		if request.path_info.match(/users\/\d+/)
+			puts "Entramos al regex"
+			puts "params=#{request.path_info.scan(/\d/)} y current=#{current_user.id}"
+			if request.path_info.scan(/\d/).first.to_i != current_user.id
+				session[:errors] = "You don't have access to this page"
+				redirect to('/')
+			end
+		end
+  end
+
+  def session_validation
+  	pass if ['login', 'register', nil].include? request.path_info.split('/')[1]
+		unless logged_in?
+			# puts "No existe una sesion"
+			session[:errors] = "No existe una sesion"
+			# @error = session[:error]
+			redirect to("/")
+		end
+  end
 end
