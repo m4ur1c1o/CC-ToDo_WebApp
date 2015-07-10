11b01a39cc36b6b322de56f32b6d3768632847e8
codeamac02 <codeamac02@gmail.com>
2015-07-10 18:55:17 -0500

    Create tasks inside current_user's profile. Check tasks as 'Done' or 'Not done' using AJAX

--- app/controllers/task.rb
+++ app/controllers/task.rb
@@ -9,3 +9,20 @@ post '/task/new' do
 	current_user.tasks << Task.create!(content: task)
 	redirect to("/users/#{current_user.id}")
 end
+
+post '/done' do
+	task_id = params[:id]
+	task = Task.find(task_id)
+	task.done = !task.done
+	
+	if task.save
+		# "Done"
+		if task.done
+			"Done"
+		else
+			"Not done"
+		end
+	else
+		"Task not updated"
+	end
+end
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,23 @@
 $(document).ready(function() {
-	// Este código corre después de que `document` fue cargado(loaded) 
-	// completamente. 
-	// Esto garantiza que si amarramos(bind) una función a un elemento 
-	// de HTML este exista ya en la página. 
+	$('.done').click(function(e){
+		e.preventDefault();
+		var task_id = $(this).parents('li').attr('id');
+		var li_task = $(this).parents('li');
+		var done_link = $(this);
+		$.ajax({
+			type: 'POST',
+			url: '/done',
+			data: { id: task_id }
+		}).done(function(data){
+			if (data == "Done") {
 
+				done_link.text("Not done");
+				// li_task.find("span.task").wrapInner("<del><del>");
+				li_task.find("span.task").toggleClass("task-done");
+			} else {
+				done_link.text("Done");
+				li_task.find("span.task").toggleClass("task-done");
+			}
+		});
+	});
 });
